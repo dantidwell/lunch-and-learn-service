2252199479c472f886909a80e805cf0a17f827b2
dantidwell <dantidwell921@gmail.com>
2015-05-26 14:43:07 -0500

    initial commit

---
+++ LunchAndLearn/LunchAndLearn.Api/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace LunchAndLearn.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("LUNCH_AND_LEARN_PORT") ?? "8080";
+            var endpoint = String.Format("http://*:{0}", port);
+            using (WebApp.Start<Startup>(endpoint))
+            {
+                Console.WriteLine("Press [enter] to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ LunchAndLearn/LunchAndLearn.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace LunchAndLearn.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                ctx.Response.ContentType = "text/plain";
+                return ctx.Response.WriteAsync("Hello, World!");
+            });
+        }
+    }
+}
