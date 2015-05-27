4ab1d210d0a4802725c61e759c2279d61e90a5b4
dantidwell <dantidwell921@gmail.com>
2015-05-26 23:17:49 -0500

    Add acceptance test project

---
+++ LunchAndLearn/LunchAndLearn.Api.AcceptanceTests/HelloWorldTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace LunchAndLearn.Api.AcceptanceTests
+{
+    [TestFixture]
+    public class HelloWorldTests
+    {
+        const string PORT = "8080";
+        const string SERVER = "localhost";
+        [Test]
+        public void ShouldRespondWithHelloWorld() {
+            var endpoint = String.Format("http://{0}:{1}", SERVER, PORT);
+            using (var client = new HttpClient() { BaseAddress = new Uri(endpoint) }) {
+                var res = client.GetAsync("api/helloworld").Result;
+                var payload = res.Content.ReadAsStringAsync();
+                Assert.AreEqual("Hello, World!", payload);
+            }
+        }
+    }
+}
