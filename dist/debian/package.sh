436c938e18408754d2600abe6bd437e44884ca1e
Ubuntu <dtidwell@dt-goci-srv.dt-goci-srv.b7.internal.cloudapp.net>
2015-05-28 15:45:21 +0000

    finish package script

--- dist/debian/package.sh
+++ dist/debian/package.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+pushd 'dist/debian'
 
-PACKAGE_ROOT='lunch-and-learn-api_$GO_PIPELINE_COUNTER'
+PACKAGE_ROOT="./lunch-and-learn-api_${GO_PIPELINE_COUNTER}"
 
 mkdir $PACKAGE_ROOT
-mkdir $PACKAGE_ROOT/usr
-mkdir $PACKAGE_ROOT/usr/bin
-mkdir $PACKAGE_ROOT/usr/lib
-mkdir $PACKAGE_ROOT/etc
-mkdir $PACKAGE_ROOT/etc/init.d
+mkdir "$PACKAGE_ROOT/usr"
+mkdir "$PACKAGE_ROOT/usr/bin"
+mkdir "$PACKAGE_ROOT/usr/lib"
+mkdir "$PACKAGE_ROOT/etc"
+mkdir "$PACKAGE_ROOT/etc/init.d"
 
-cp "../LunchAndLearn/LunchAndLearn.Api/bin/$CONFIGURATION/.*' '$PACKAGE_ROOT/usr/lib'
+cp -a "./../../LunchAndLearn/LunchAndLearn.Api/bin/${CONFIGURATION}/." "${PACKAGE_ROOT}/usr/lib"
+mkdir "$PACKAGE_ROOT/DEBIAN"
+echo "Package: lunch-and-learn-api" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Version: ${GO_PIPELINE_COUNTER}" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Section: base" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Priority: optional" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Architecture: x64" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Depends: " >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Maintainer: Dan Tidwell <dantidwell921@gmail.com>" >> "${PACKAGE_ROOT}/DEBIAN/control"
+echo "Description: Lunch and Learn Web Service" >> "${PACKAGE_ROOT}/DEBIAN/control"
 
-
+dpkg-deb --build "lunch-and-learn-api_${GO_PIPELINE_COUNTER}"
+popd
